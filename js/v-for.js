270c26cc0d387daa983e9933da180aa81518a5fb
wzp <742349899@qq.com>
2019-01-20 09:09:33 +0800

    Vue

---
+++ js/v-for.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:"#vue-app",
+    data:{
+        characters:["mike","hery","locy"],
+        person:[
+            {name:"wzp", age:18},
+            {name:"lbf", age:16},
+            {name:"lbl", age:12}
+        ]
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+});
